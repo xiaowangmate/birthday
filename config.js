296b357237500d8ac5001017d2363aa577e3c3a6
xiaowangmate <125385273+xiaowangmate@users.noreply.github.com>
2023-07-07 23:58:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,18 +12,19 @@ var config = {
         "让你成为饱贝",
         "mua~~",
     ],
-    imgs: 
-        //"心爱的小可爱": "./imgs/xiaokeai.png",
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始咯",
-        play: "放点音乐",
-        bannar_coming: "来点色彩",
+        turn_on: "开始",
+        play: "音乐",
+        bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "For my baby！",
+        story: "A MESSAGE FOR YOU",
     }
 };
