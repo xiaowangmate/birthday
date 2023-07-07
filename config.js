37c18254f6eb5f8fe8e89bb0b51222082df37818
xiaowangmate <125385273+xiaowangmate@users.noreply.github.com>
2023-07-08 00:31:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ var config = {
         "但我用这个网页把祝福和爱传递给你",
         "小可爱",
         "我一直是最爱你的呀",
-        "所以要赚很多很多的钱把你喂得饱饱的",
+        "要赚很多很多的钱把你喂得饱饱的",
         "让你成为饱贝",
         "mua~~",
     ],
@@ -24,19 +24,19 @@ var config = {
      * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
-    imgs: 
-        //"心爱的小宝贝": "./imgs/xiaokeai.png",
+    imgs: {
+        "心爱的小宝贝": "./imgs/baby.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        turn_on: "怎么黑乎乎的？",
+        play: "这有音乐",
+        bannar_coming: "这是什么？",
+        balloons_flying: "点一下哦",
+        cake_fadein: "宝贝的蛋糕",
+        light_candle: "点上蜡烛吧",
+        wish_message: "小马同学，生日快乐！",
+        story: "For My Baby!",
     }
 };
