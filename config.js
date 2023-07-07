08497b3cb3f570bb03c43f88f4d2795c84f9faaf
xiaowangmate <125385273+xiaowangmate@users.noreply.github.com>
2023-07-07 23:50:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ var config = {
         "今天是你的生日",
         "生日快乐宝贝",
         "虽然不能过来给你过生日",
-        "但我用这个生日网页把祝福和爱传递给你",
+        "但我用这个网页把祝福和爱传递给你",
         "小可爱",
         "我一直是最爱你的呀",
-        "要赚很多很多的钱把你喂得饱饱的",
+        "所以要赚很多很多的钱把你喂得饱饱的",
         "让你成为饱贝",
         "mua~~",
     ],
@@ -25,18 +25,18 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: 
-        "心爱的小可爱": "./imgs/baby.jpg",
+        "心爱的小宝贝": "./imgs/baby.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        turn_on: "开始咯",
+        play: "放点音乐",
+        bannar_coming: "来点色彩",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        light_candle: "蜡烛？"
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        story: "For my baby！"
     }
 };
