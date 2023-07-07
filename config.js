cf9c1adf55b2f8dd1e329b3de13326cb806fc5d1
xiaowangmate <125385273+xiaowangmate@users.noreply.github.com>
2023-07-08 00:07:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -31,8 +31,8 @@ var config = {
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始咯",
-        play: "音乐",
-        bannar_coming: "颜色",
+        play: "放点音乐",
+        bannar_coming: "来点色彩",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
